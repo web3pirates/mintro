5da5f8c0528aa06a0cf28721e4e82d531da1ae8d
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-07-05 11:25:22 +0200

    worldcoin-mini-app: add versioning

--- mintro-mini-app/src/components/MintroBranding/index.tsx
+++ mintro-mini-app/src/components/MintroBranding/index.tsx
@@ -95,7 +95,7 @@ export const MintroBranding = () => {
             Mintro
           </h1>
           <p className="text-xl md:text-2xl text-white max-w-3xl mx-auto">
-            Your intelligent DeFi companion.
+            Your intelligent DeFi companion v0.1
           </p>
         </div>
       </div>
