9b8e0fdd61e4705f15cc56845fb45b7ab9011cc9
Marco Roccon <marco.roccon@gmail.com>
2025-07-06 01:13:04 +0200

    backend: add dca route in mintro-mini-app

--- backend/routes/dca.js
+++ backend/routes/dca.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { executeDCA } = require('../dca-engine');
+const { executeDCA } = require('../app/automation/dca-engine');
 
 router.post('/execute', auth, async (req, res) => {
   try {
